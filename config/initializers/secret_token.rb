d60a61cf23505d56af72e90e0c79300bd0c97ed0
Navid Mostafavi <navidm@gmail.com>
2014-02-21 15:30:44 -0800

    created project, swithced db to postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gothere::Application.config.secret_key_base = '895fff5159ceb20d15be22fd95e04ac23636b720cd1d39313f19ef53cd914e7f047179cf9d055d241eb89d5f21527fbf784db1edbd31eac60c76aff7d46ddf88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gothere::Application.config.session_store :cookie_store, key: '_gothere_session'
