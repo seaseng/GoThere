a920ceb945dc7f8f9420726a63772a7cbe095367
nathanallen <thenathanator@gmail.com>
2014-02-22 14:21:06 -0800

    display crime index layer

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -11,10 +11,12 @@ var doOnLoad = function() {
     "esri/graphic", 
     "esri/symbols/SimpleMarkerSymbol", 
     "dojo/_base/Color",
+    "esri/layers/ArcGISDynamicMapServiceLayer",
+    "esri/layers/ImageParameters",
     "dojo/domReady!"
   ],  
-    function(Map, Point, Graphic, SimpleMarkerSymbol, Color){
-      MapController.init(Map, Point, Graphic, SimpleMarkerSymbol, Color)
+    function(Map, Point, Graphic, SimpleMarkerSymbol, Color, ArcGISDynamicMapServiceLayer, ImageParameters){
+      MapController.init(Map, Point, Graphic, SimpleMarkerSymbol, Color, ArcGISDynamicMapServiceLayer, ImageParameters)
     }
   )
 }
@@ -23,17 +25,19 @@ var doOnLoad = function() {
 MapController = {
   pointer_icon: "M16,3.5c-4.142,0-7.5,3.358-7.5,7.5c0,4.143,7.5,18.121,7.5,18.121S23.5,15.143,23.5,11C23.5,6.858,20.143,3.5,16,3.5z M16,14.584c-1.979,0-3.584-1.604-3.584-3.584S14.021,7.416,16,7.416S19.584,9.021,19.584,11S17.979,14.584,16,14.584z",
   map_center: [-122.44,37.77], //lon, lat
-  init: function(Map, Point, Graphic, SimpleMarkerSymbol, Color){
+  init: function(Map, Point, Graphic, SimpleMarkerSymbol, Color, ArcGISDynamicMapServiceLayer, ImageParameters){
     this.Point = Point
     this.Graphic = Graphic
-    esriConfig.defaults.io.proxyUrl = ""
     this.buildMarkerObject(SimpleMarkerSymbol,Color)
     var that = this;
     this.render(Map, function(){
       that.getData(function(json){
         that.placeMarkers(json)
       })
+      that.renderLayer(ImageParameters, ArcGISDynamicMapServiceLayer)
     })
+
+
   },
   render: function(Map, callback){
     this.map =  new Map("map", {
@@ -43,6 +47,18 @@ MapController = {
       sliderStyle: "small"
     });
     callback()
+  },
+  renderLayer: function(ImageParameters, ArcGISDynamicMapServiceLayer){
+    var imageParameters = new ImageParameters();
+    imageParameters.format = "jpeg"; //set the image type to PNG24, note default is PNG8.
+
+    //Takes a URL to a non cached map service.
+    var dynamicMapServiceLayer = new ArcGISDynamicMapServiceLayer("http://megacity.esri.com/ArcGIS/rest/services/Demographics/USA_CrimeIndex/MapServer", {
+      "opacity" : 0.8,
+      "imageParameters" : imageParameters
+    });
+
+    this.map.addLayer(dynamicMapServiceLayer);
   },
   getData: function(callback){
     $.get("http://data.sfgov.org/resource/gxxq-x39z.json")
