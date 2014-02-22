2d6cca4050b33a21323b799a798659655f158295
Navid Mostafavi <navidm@gmail.com>
2014-02-21 22:33:36 -0800

    fixed factory error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,6 @@ describe User do
   let!(:user) {create(:user)}
   it { should validate_presence_of(:email)}  
   it "should have correct saved email" do
-    expect(user.email).to eq 'test1@test.com'
+    expect(user.email).to include '@test.com'
   end
 end
