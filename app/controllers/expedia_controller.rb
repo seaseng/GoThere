c19f4f2c54b41ba31fd4363da0919ca961b4f98b
stomatocode <dewrgleision@gmail.com>
2014-02-22 14:56:23 -0800

    api call with test params works for vienna

--- app/controllers/expedia_controller.rb
+++ app/controllers/expedia_controller.rb
@@ -1,25 +1,22 @@
 class ExpediaController < ApplicationController
+  
   def new
     require "expedia"
-      
+    
     Expedia.cid = 55505
-    Expedia.api_key = 'xxxx'
-    Expedia.shared_secret = 'xxxx'
-    Expedia.locale = 'en_US'
+    Expedia.api_key = ENV['EXPEDIA_KEY']
+    Expedia.shared_secret = ENV['EXPEDIA_SECRET']
+    # Expedia.locale = 'en_US'
     Expedia.currency_code = 'USD'
     Expedia.minor_rev = 13
 
     api = Expedia::Api.new
 
-    response = api.get_list({address: "30 Germania St, Boston, MA 02130}"})
+    response = api.get_list({city: "Vienna", useGeoCoder: true, minTripAdvisorRating: 3.0,})
 
-    response = api.get_list({arrivalDate:"02/22/2014 ",destinationString: "San Francisco, CA USA", numberOfResults: 30, searchradius: 10, sort: "PROMO"})
-    #
-    # expect JSON response
-    # render JSON 
+    # response = api.get_list({arrivalDate:"02/22/2014 ",destinationString: "San Francisco, CA USA", numberOfResults: 30, searchradius: 10, sort: "PROMO"})
+
     render :json => response.to_json
   end
 
-  
-  
 end
