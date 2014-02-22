2551bc66d513916740b0bb4a8b900a3b9b0b21f4
nathanallen <thenathanator@gmail.com>
2014-02-22 13:25:30 -0800

    create testData.js file for development

---
+++ app/assets/javascripts/testData.js
@@ -0,0 +1,130 @@
+TestData = {
+  place: {
+    coords: {
+      x: -82.59, 
+      y: 39.59
+    }, attributes: {
+      address: "100 Street",
+      price: "$2.50"
+    }
+  },
+  places: [
+    this.place,
+    {coords: {
+      x: -122.59, 
+      y: 39.59
+    }, attributes: {
+      address: "200 Street",
+      price: "$12.50"
+    }}
+  ],
+
+  crimes: [{
+    "time" : "12:00",
+    "category" : "LARCENY/THEFT",
+    "pddistrict" : "CENTRAL",
+    "location" : {
+      "needs_recoding" : false,
+      "longitude" : "-122.419616109671",
+      "latitude" : "37.8019942438049"
+    },
+    "address" : "LOMBARD ST / HYDE ST",
+    "descript" : "GRAND THEFT FROM LOCKED AUTO",
+    "dayofweek" : "Friday",
+    "resolution" : "NONE",
+    "date" : 1391155200,
+    "y" : "37.8019942438049",
+    "x" : "-122.419616109671",
+    "incidntnum" : "146022613"
+  }
+  , {
+    "time" : "13:55",
+    "category" : "LARCENY/THEFT",
+    "pddistrict" : "CENTRAL",
+    "location" : {
+      "needs_recoding" : false,
+      "longitude" : "-122.405894420706",
+      "latitude" : "37.7896023539705"
+    },
+    "address" : "300 Block of SUTTER ST",
+    "descript" : "GRAND THEFT FROM LOCKED AUTO",
+    "dayofweek" : "Friday",
+    "resolution" : "NONE",
+    "date" : 1391155200,
+    "y" : "37.7896023539705",
+    "x" : "-122.405894420706",
+    "incidntnum" : "146022362"
+  }
+  , {
+    "time" : "17:15",
+    "category" : "BURGLARY",
+    "pddistrict" : "TENDERLOIN",
+    "location" : {
+      "needs_recoding" : false,
+      "longitude" : "-122.409973312269",
+      "latitude" : "37.7851896203434"
+    },
+    "address" : "200 Block of ELLIS ST",
+    "descript" : "BURGLARY,STORE UNDER CONSTRUCTION, FORCIBLE ENTRY",
+    "dayofweek" : "Friday",
+    "resolution" : "NONE",
+    "date" : 1391155200,
+    "y" : "37.7851896203434",
+    "x" : "-122.409973312269",
+    "incidntnum" : "140098935"
+  }
+  , {
+    "time" : "14:00",
+    "category" : "LARCENY/THEFT",
+    "pddistrict" : "CENTRAL",
+    "location" : {
+      "needs_recoding" : false,
+      "longitude" : "-122.409043928994",
+      "latitude" : "37.7975730481122"
+    },
+    "address" : "BROADWAY ST / CORDELIA ST",
+    "descript" : "GRAND THEFT FROM UNLOCKED AUTO",
+    "dayofweek" : "Friday",
+    "resolution" : "NONE",
+    "date" : 1391155200,
+    "y" : "37.7975730481122",
+    "x" : "-122.409043928994",
+    "incidntnum" : "140091785"
+  }
+  , {
+    "time" : "02:00",
+    "category" : "LARCENY/THEFT",
+    "pddistrict" : "MISSION",
+    "location" : {
+      "needs_recoding" : false,
+      "longitude" : "-122.418601780502",
+      "latitude" : "37.7636325572274"
+    },
+    "address" : "100 Block of CAPP ST",
+    "descript" : "GRAND THEFT FROM LOCKED AUTO",
+    "dayofweek" : "Friday",
+    "resolution" : "NONE",
+    "date" : 1391155200,
+    "y" : "37.7636325572274",
+    "x" : "-122.418601780502",
+    "incidntnum" : "146023229"
+  }
+  , {
+    "time" : "14:48",
+    "category" : "NON-CRIMINAL",
+    "pddistrict" : "NORTHERN",
+    "location" : {
+      "needs_recoding" : false,
+      "longitude" : "-122.420676028352",
+      "latitude" : "37.7762540028806"
+    },
+    "address" : "100 Block of FELL ST",
+    "descript" : "DEATH REPORT, CAUSE UNKNOWN",
+    "dayofweek" : "Friday",
+    "resolution" : "NONE",
+    "date" : 1391155200,
+    "y" : "37.7762540028806",
+    "x" : "-122.420676028352",
+    "incidntnum" : "140091848"
+  }]
+}
