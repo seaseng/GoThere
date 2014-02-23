76f331597b0095e6e0d4280b4e39b1ec78ee7ee0
nathanallen <thenathanator@gmail.com>
2014-02-22 19:36:20 -0800

    hotel pins on map, map recenters/zooms on search

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -22,7 +22,7 @@ var doOnLoad = function() {
 //global
 MapController = {
   pointer_icon: "M16,3.5c-4.142,0-7.5,3.358-7.5,7.5c0,4.143,7.5,18.121,7.5,18.121S23.5,15.143,23.5,11C23.5,6.858,20.143,3.5,16,3.5z M16,14.584c-1.979,0-3.584-1.604-3.584-3.584S14.021,7.416,16,7.416S19.584,9.021,19.584,11S17.979,14.584,16,14.584z",
-  map_center: [-122.44,37.77], //lon, lat
+  map_center: [-98.35,39.50], //lon, lat
   init: function(Map, Point, Graphic, SimpleMarkerSymbol, Color){
     this.Point = Point
     this.Graphic = Graphic
@@ -38,21 +38,34 @@ MapController = {
     this.map =  new Map("map", {
       basemap: "streets", 
       center: this.map_center,
-      zoom: 13,
+      zoom: 4,
       sliderStyle: "small"
     });
     callback()
   },
+  recenter: function(lonlat){
+    this.map.centerAndZoom(lonlat,13)
+  },
   getData: function(callback){
-    $.get("http://data.sfgov.org/resource/gxxq-x39z.json")
-     .done(callback)
+    // $.get("http://data.sfgov.org/resource/gxxq-x39z.json")
+    //  .done(callback)
   },
   buildMarkerObject: function(SimpleMarkerSymbol, Color){
     this.symbol = new SimpleMarkerSymbol({
       //style: 'circle',
-      //color: new Color([153,0,51,0.75]),
+      color: new Color([36,109,198,1]),
     })
     this.symbol.setPath(this.pointer_icon)
+  },
+  placeHotelMarkers: function(arr){
+    this.recenter([arr[0].longitude, arr[0].latitude])
+
+    var that = this;
+    arr.forEach(function(place){
+      var coords = {x: place.longitude, y: place.latitude}
+      var attributes = {street_address: place.address1}
+      that.placeMarker({coords: coords, attributes: attributes})
+    })
   },
   placeMarkers: function(arr){
     var that = this;
